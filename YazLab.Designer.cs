fddcdb33d34d11107108f843037474e7f9b464f0
Yavuz Melih Özgüven <yavuzozguven@hotmail.com>
2018-12-28 14:55:51 +0300

    -

---
+++ YazLab.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace YazLab
+{
+    partial class YazLab
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YazLab));
+            this.Dosyaac = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Dosya_yolu = new System.Windows.Forms.Label();
+            this.resim = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.en_al = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.boy_al = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.play_stop = new System.Windows.Forms.Button();
+            this.format = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.goster = new System.Windows.Forms.Button();
+            this.frame_label = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Dosyaac
+            // 
+            this.Dosyaac.Image = ((System.Drawing.Image)(resources.GetObject("Dosyaac.Image")));
+            this.Dosyaac.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Dosyaac.Location = new System.Drawing.Point(12, 53);
+            this.Dosyaac.Name = "Dosyaac";
+            this.Dosyaac.Size = new System.Drawing.Size(133, 28);
+            this.Dosyaac.TabIndex = 1;
+            this.Dosyaac.Text = "Dosya Aç";
+            this.Dosyaac.UseVisualStyleBackColor = true;
+            this.Dosyaac.Click += new System.EventHandler(this.Dosyaac_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(788, 46);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Indigo;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Indigo;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(760, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 28);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(3, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "YUV CONVERTER";
+            // 
+            // Dosya_yolu
+            // 
+            this.Dosya_yolu.AutoSize = true;
+            this.Dosya_yolu.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Dosya_yolu.Location = new System.Drawing.Point(9, 93);
+            this.Dosya_yolu.Name = "Dosya_yolu";
+            this.Dosya_yolu.Size = new System.Drawing.Size(0, 13);
+            this.Dosya_yolu.TabIndex = 3;
+            // 
+            // resim
+            // 
+            this.resim.Location = new System.Drawing.Point(250, 53);
+            this.resim.Name = "resim";
+            this.resim.Size = new System.Drawing.Size(386, 339);
+            this.resim.TabIndex = 4;
+            this.resim.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(12, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Bmp Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // en_al
+            // 
+            this.en_al.Location = new System.Drawing.Point(12, 191);
+            this.en_al.Name = "en_al";
+            this.en_al.Size = new System.Drawing.Size(50, 20);
+            this.en_al.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "En:";
+            // 
+            // boy_al
+            // 
+            this.boy_al.Location = new System.Drawing.Point(12, 254);
+            this.boy_al.Name = "boy_al";
+            this.boy_al.Size = new System.Drawing.Size(50, 20);
+            this.boy_al.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Boy:";
+            // 
+            // play_stop
+            // 
+            this.play_stop.FlatAppearance.BorderSize = 0;
+            this.play_stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.play_stop.Image = ((System.Drawing.Image)(resources.GetObject("play_stop.Image")));
+            this.play_stop.Location = new System.Drawing.Point(410, 398);
+            this.play_stop.Name = "play_stop";
+            this.play_stop.Size = new System.Drawing.Size(65, 46);
+            this.play_stop.TabIndex = 10;
+            this.play_stop.UseVisualStyleBackColor = true;
+            this.play_stop.Click += new System.EventHandler(this.play_stop_Click);
+            // 
+            // format
+            // 
+            this.format.FormattingEnabled = true;
+            this.format.Items.AddRange(new object[] {
+            "4:4:4",
+            "4:2:2",
+            "4:2:0"});
+            this.format.Location = new System.Drawing.Point(12, 323);
+            this.format.Name = "format";
+            this.format.Size = new System.Drawing.Size(74, 21);
+            this.format.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(12, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Format:";
+            // 
+            // goster
+            // 
+            this.goster.Location = new System.Drawing.Point(12, 398);
+            this.goster.Name = "goster";
+            this.goster.Size = new System.Drawing.Size(75, 23);
+            this.goster.TabIndex = 13;
+            this.goster.Text = "Göster";
+            this.goster.UseVisualStyleBackColor = true;
+            this.goster.Click += new System.EventHandler(this.goster_Click);
+            // 
+            // frame_label
+            // 
+            this.frame_label.AutoSize = true;
+            this.frame_label.Location = new System.Drawing.Point(551, 415);
+            this.frame_label.Name = "frame_label";
+            this.frame_label.Size = new System.Drawing.Size(0, 13);
+            this.frame_label.TabIndex = 14;
+            // 
+            // YazLab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.frame_label);
+            this.Controls.Add(this.goster);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.format);
+            this.Controls.Add(this.play_stop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.boy_al);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.en_al);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.resim);
+            this.Controls.Add(this.Dosya_yolu);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Dosyaac);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "YazLab";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YazLab";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Dosyaac;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Dosya_yolu;
+        private System.Windows.Forms.PictureBox resim;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox en_al;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox boy_al;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button play_stop;
+        private System.Windows.Forms.ComboBox format;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button goster;
+        private System.Windows.Forms.Label frame_label;
+    }
+}
+
---
+++ YazLab.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YazLab
+{
+    public partial class YazLab : Form
+    {
+        
+        OpenFileDialog op = new OpenFileDialog();
+        int boyut,frame,max_frame,en,boy,sayac=0;
+        byte[] y, u, v;
+        Bitmap bmp;
+        FileStream fs;
+        BinaryReader br;
+
+        public YazLab()
+        {
+            InitializeComponent();
+        }
+
+        private void Dosyaac_Click(object sender, EventArgs e)
+        {
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fi = new FileInfo(op.FileName);
+                
+                
+                
+                
+                if (fi.Exists)
+                {
+                    Dosya_yolu.Text = fi.Name;
+                }
+                else
+                {
+                    Dosya_yolu.Text = "Dosya açılamadı.";
+                }
+            }
+        }
+
+        private void play_stop_Click(object sender, EventArgs e)
+        {
+            
+            frame = 1;
+            while (frame != max_frame)
+            {
+                String path = "C:\\Users\\Yavuz\\Desktop\\Yuvoutput\\deneme" + frame + ".bmp";
+                frame_label.Text = frame + "/" + max_frame;
+                frame_label.Update();
+                Thread.Sleep(200);
+                resim.Image = new Bitmap(path);
+                resim.Update();
+                frame++;
+            }
+            frame_label.Text = frame + "/" + max_frame;
+            frame_label.Update();
+        }
+
+        public void yuv2rgb(byte[] y,byte[] u,byte []v,int boyut)
+        {
+            byte[] r = new byte[boyut];
+            byte[] g = new byte[boyut];
+            byte[] b = new byte[boyut];
+
+            for (int i = 0; i < boyut; i++)
+            {
+                //r[i] =(byte)(y[i] + (114*v[i]/100));
+                //r[i] = (byte)(1.164*(y[i]-16) +  1.596*(v[i]-128));
+                r[i] = (byte)(1.164 * (y[i] - 16));
+                if (r[i] > 255)
+                    r[i] = 255;
+                if(r[i] < 0)
+                {
+                    r[i] = 0;
+                }
+                //g[i] =(byte) (y[i] - (u[i]*0.395) - (v[i]*0.581));
+                //g[i] = (byte)(1.164*(y[i]-16) - ((u[i]-128) * 0.391) - ((v[i]-128) * 0.813));
+                g[i] = (byte)(1.164 * (y[i] - 16));
+                if (g[i] > 255)
+                    g[i] = 255;
+                if (g[i] < 0)
+                {
+                    g[i] = 0;
+                }
+                //b[i] =(byte) (1.164*(y[i]-16) + (2.018 * (u[i]-128)));
+                b[i] = (byte)(1.164 * (y[i] - 16));
+                if (b[i] > 255)
+                    b[i] = 255;
+                if (b[i] < 0)
+                {
+                    b[i] = 0;
+                }
+            }
+            rgb2bmp(r, g, b);
+            //BytesToBitmap(r,g,b);
+        }
+
+        public Bitmap rgb2bmp(byte[] r,byte[] g,byte []b)
+        {
+            bmp = new Bitmap(en, boy, PixelFormat.Format24bppRgb);
+            Rectangle rect = new Rectangle(0, 0, en, boy);
+            BitmapData bmpd = bmp.LockBits(rect, ImageLockMode.WriteOnly, bmp.PixelFormat);
+            int padding = bmpd.Stride - 3 * en;
+            unsafe
+            {
+                byte* ptr = (byte*)bmpd.Scan0;
+                for (int i = 0; i < boy; i++)
+                {
+                    for (int j = 0; j < en; j++)
+                    {
+                        ptr[2] =  r[i * en + j];
+                        ptr[1] =  g[i * en + j];
+                        ptr[0] =  b[i * en + j];
+                        ptr += 3;
+                    }
+                    ptr += padding;
+                }
+            }
+            bmp.UnlockBits(bmpd);
+            resim.SizeMode = PictureBoxSizeMode.CenterImage;
+            
+            resim.SizeMode = PictureBoxSizeMode.StretchImage;
+            
+            resim.Image = bmp;
+            sayac++;
+            bmp.Save("C:\\Users\\Yavuz\\Desktop\\Yuvoutput\\deneme"+sayac+".bmp");
+            return bmp;
+        }
+
+        
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.Filter = "Bitmap |*.bmp";
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                bmp.Save(sf.FileName,ImageFormat.Bmp);
+            }
+        }
+
+        private void goster_Click(object sender, EventArgs e)
+        {
+            boyut = Int32.Parse(en_al.Text) * Int32.Parse(boy_al.Text);
+            en = Int32.Parse(en_al.Text);
+            boy = Int32.Parse(boy_al.Text);
+            
+            u = new byte[boyut];
+            v = new byte[boyut];
+            int cnt = 0;
+
+                fs = new FileStream(op.FileName, FileMode.Open);
+                br = new BinaryReader(fs);
+                
+
+                frame = 0;
+
+                if (format.SelectedIndex == 0)
+                {
+                y = new byte[boyut*3];
+                for (int i = 0; i < fs.Length; i += boyut)
+                    {
+                        if (i % boyut == 0)
+                            max_frame++;
+                    }
+                    max_frame = max_frame / 3;
+
+                while (cnt != max_frame)
+                {
+                    for (int i = 0; i < boyut*3; i++)
+                    { 
+                        y[i] = br.ReadByte();
+                    }
+                    cnt++;
+                    yuv2rgb(y, u, v, boyut);
+                    frame++;
+                    frame_label.Text = (frame) + "/" + max_frame;
+                }
+                
+                }
+                if (format.SelectedIndex == 1)
+                {
+                    byte[] y = new byte[boyut*2];
+                    
+                    for (int i = 0; i < fs.Length; i += boyut)
+                    {
+                        if (i % boyut == 0)
+                            max_frame++;
+                    }
+                    max_frame = (int) (max_frame / 3 * 1.5);
+                while (cnt != max_frame)
+                {
+                    for (int i = 0; i < boyut * 2; i++)
+                    {
+                        y[i] = br.ReadByte();
+                    }
+                    cnt++;
+                    yuv2rgb(y, u, v, boyut);
+                    frame++;
+                    frame_label.Text = (frame) + "/" + max_frame;
+                }
+                }
+                if (format.SelectedIndex == 2)
+                {
+                y = new byte[boyut * 3];
+                for (int i = 0; i < fs.Length; i += boyut)
+                    {
+                        if (i % boyut == 0)
+                            max_frame++;
+                    }
+                    max_frame = max_frame / 3 * 2;
+
+                
+                while (cnt != max_frame)
+                {
+                    for (int i = 0; i < boyut * 3 / 2; i++)
+                    {
+                        y[i] = br.ReadByte();
+                    }
+                    cnt++;
+                    yuv2rgb(y, u, v, boyut);
+                    frame++;
+                    frame_label.Text = (frame) + "/" + max_frame;
+                }
+                
+
+                fs.Close();
+               
+                }
+
+
+            
+            //fs.Close();
+           
+
+        }
+
+        
+
+        
+    }
+}
